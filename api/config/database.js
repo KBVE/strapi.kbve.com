2424f4fef1b1131c2f878603762910042263e7cf
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-08-09 00:47:08 -0400

    Docker Init.

--- api/config/database.js
+++ api/config/database.js
@@ -2,10 +2,15 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
     },
     useNullAsDefault: true,
+    debug: false,
   },
 });
