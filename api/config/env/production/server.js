6810b9a2f4f7e6962b08ed09670e2e5e198fda3c
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-20 00:50:52 -0400

    Updates to the env.
    Co-Authored-By: J. Zane Cook <90814067+jzanecook@users.noreply.github.com>

--- api/config/env/production/server.js
+++ api/config/env/production/server.js
@@ -2,9 +2,9 @@
 const secrets = require('./secrets');
 
 module.exports = ({ env }) => ({
-  host: secrets.read($HOST_FILE) || env('HOST', '0.0.0.0'),
-  port: secrets.read($PORT_FILE) || env.int('PORT', 1337),
+  host: secrets.read(process.env.HOST_FILE) || env('HOST', '0.0.0.0'),
+  port: secrets.read(process.env.PORT_FILE) || env.int('PORT', 1337),
   app: {
-    keys: secrets.read($APP_KEYS_FILE) || env.array('APP_KEYS'),
+    keys: secrets.read(process.env.APP_KEYS_FILE) || env.array('APP_KEYS'),
   },
 });
