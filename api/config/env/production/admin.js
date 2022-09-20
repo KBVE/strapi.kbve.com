08f3bcc49b0a97d3b5434dba25010971924bd5a7
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-20 03:12:01 -0400

    Update admin.js

--- api/config/env/production/admin.js
+++ api/config/env/production/admin.js
@@ -6,4 +6,11 @@ module.exports = ({ env }) => ({
     //secret: secrets.read($ADMIN_JWT_SECRET_FILE) ||  env('ADMIN_JWT_SECRET', '5c60d7e83951c8844ba088ef68bff791'),
     secret: secrets.read(env('ADMIN_JWT_SECRET_FILE')) ||  env('ADMIN_JWT_SECRET', '5c60d7e83951c8844ba088ef68bff791'),
   },
+  
+  apiToken: {
+    // This needs to be updated, but for now, we will use the JWT secret again.
+    salt: secrets.read(env('ADMIN_JWT_SECRET_FILE')) ||  env('ADMIN_JWT_SECRET', '5c60d7e83951c8844ba088ef68bff791'),
+  },
+  url: env('PUBLIC_ADMIN_URL', '/dashboard'),
+  serveAdminPanel: env.bool('SERVE_ADMIN', true),
 });
