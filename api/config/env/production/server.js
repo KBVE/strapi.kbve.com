c09f1f6edefcc188541c17a59cc6bc1ed6b1f5db
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-09-20 19:10:14 -0400

    Missing an entity in env.

--- api/config/env/production/server.js
+++ api/config/env/production/server.js
@@ -5,6 +5,6 @@ module.exports = ({ env }) => ({
   host: secrets.read(env('HOST_FILE')) || env('HOST', '0.0.0.0'),
   port: secrets.read(env('PORT_FILE')) || env.int('PORT', 1337),
   app: {
-    keys: secrets.read(env('APP_KEYS_FILE')) || env.array('APP_KEYS'),
+    keys: secrets.read(env.array('APP_KEYS_FILE')) || env.array('APP_KEYS'),
   },
 });
