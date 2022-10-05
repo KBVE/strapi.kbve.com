ad256fe31d2e1292c252bf23a5646090700747a0
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2022-10-05 02:40:19 -0400

    Captcha
    - Captcha should work but needs some work on the error system, ideally to integrate it with the Strapi error system.

--- api/src/extensions/users-permissions/strapi-server.js
+++ api/src/extensions/users-permissions/strapi-server.js
@@ -1,5 +1,6 @@
-//const axios = require("axios");
+// Import hCaptcha library
 const {verify} = require('hcaptcha');
+// Module <Plugin> overwrite Register
 module.exports = (plugin) => {
   const register = plugin.controllers.auth.register;
 
@@ -7,62 +8,17 @@ module.exports = (plugin) => {
     ctx.request.body.confirmed = false;
     const token = ctx.request.body.token;
     const hcaptcha = strapi.config.get('server.hcaptcha');
-    console.log('Test Case');
 
-    //const hcaptcha = env.hcaptcha;
-
-    /*
-        const hcaptcha_feedback = await axios.post(
-          'https://hcaptcha.com/siteverify', {
-            'secret': hcaptcha,
-            'response': token
-          }
-        );
-    */
-
-    // Verify is from the [NPMJS Package -> hcaptcha] || [yarn add hcaptcha]
-    console.log('----- [Hcaptcha] [START]');
-    console.log(hcaptcha);
-    console.log('----- [Hcaptcha] [END]');
-    console.log('------------------');
-    console.log('----- [Token] [START]');
-    console.log(token);
-    console.log('----- [Token] [END]');
-
-    try {
-
-      // let {
-      //     success,
-      //     challenge_ts,
-      //     hostname,
-      //     credit,
-      //   } = await verify(hcaptcha, token)
-
-
-      let result = await verify(hcaptcha, token)
-
-      console.log(result);
-
-      if (result.success) {
-
-        console.log('success!', data);
-        register(ctx);
-        //  ctx.send({ message: 'success' });
-        //  next();
-      } else {
-
-        console.log(result.error);
-        console.log(result.challenge_ts);
-        console.log(result.hostname);
-        console.log('failed at line 57');
-
-        return ctx.throw(500, 'Verify Order 44. Failed')
-      }
-    }
-      catch (error) {
-        return ctx.throw(418, 'Captcha not verified')
-    }
-
+    let result = await verify(hcaptcha, token)
+              if(result.success!==true)
+              {
+                return ctx.badRequest(null, 'hCaptcha Verification had an issue. ')
+              }
+              else
+              {
+              await register(ctx);
+              }
   };
+
   return plugin;
 };
