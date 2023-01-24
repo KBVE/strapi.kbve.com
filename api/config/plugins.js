f47a6c3f42d8b442a94a641389a0a1506c7a45b6
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-01-24 11:12:12 -0500

    Prometheus

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -6,5 +6,38 @@ module.exports = ({ env }) => ({
     config: {
       jwtSecret: secrets.read(env('JWT_SECRET_FILE')) ||  env('JWT_SECRET', '5c60d7e83951c8844ba088ef68bff791'),
     }
+  },
+  'strapi-prometheus': {
+    enabled: true,
+    config: {
+      // add prefix to all the prometheus metrics names.
+      prefix: '',
+
+      // use full url instead of matched url
+      // true  => path label: `/api/models/1`
+      // false => path label: `/api/models/:id`
+      fullURL: false,
+
+      // include url query in the url label
+      // true  => path label: `/api/models?limit=1`
+      // false => path label: `/api/models`
+      includeQuery: false,
+
+      // metrics that will be enabled, by default they are all enabled.
+      enabledMetrics: {
+        koa: true, // koa metrics
+        process: true, // metrics regarding the running process
+        http: true, // http metrics like response time and size
+        apollo: true // metrics regarding graphql
+      },
+
+      // interval at which rate metrics are collected in ms
+      interval: 10_000,
+
+      // set custom/default labels to all the prometheus metrics
+      customLabels: {
+        name: "strapi-prometheus",
+      },
+    }
   }
 })
